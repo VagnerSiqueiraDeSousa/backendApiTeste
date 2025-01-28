97bc01a019f0205a04860f17380a2e2cf13acb1a
VagnerSousa <vagner.s.sousa@outlook.com>
2025-01-28 19:23:13 -0300

    otimizando os endpoint get e post

--- criandoapi/criandoapi/src/main/java/br/com/seteu/criandoapi/DAO/IUsuario.java
+++
@@ -1,7 +0,0 @@
-package br.com.seteu.criandoapi.DAO;
-
-import br.com.seteu.criandoapi.model.Usuario;
-import org.springframework.data.repository.CrudRepository;
-
-public interface IUsuario extends CrudRepository<Usuario, Integer> {
-}
---
+++ criandoapi/criandoapi/src/main/java/br/com/seteu/criandoapi/service/UsuarioService.java
@@ -0,0 +1,28 @@
+package br.com.seteu.criandoapi.service;
+
+import br.com.seteu.criandoapi.model.Usuario;
+import br.com.seteu.criandoapi.repository.IUsuario;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UsuarioService {
+
+    private IUsuario repository;
+
+    public UsuarioService(IUsuario repository){
+        this.repository = repository;
+    }
+
+    public List<Usuario> ListarUsuario(){
+        List<Usuario> lista = repository.findAll();
+        return lista;
+    }
+
+    public Usuario criarUsuario(Usuario usuario){
+        Usuario usuarioNovo = repository.save(usuario);
+        return usuarioNovo;
+    }
+
+}
